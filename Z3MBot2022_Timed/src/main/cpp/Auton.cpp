0434bebd0d9b86ec28fdd9c0f143a04b4717123b
Govind Suresh <govindsuresh2001@gmail.com>
2022-02-10 02:26:16 -0500

    Add DriveDistance and TurnAngle functions to change speed and return true when distance/angle has been reached.

--- Z3MBot2022_Timed/src/main/cpp/Auton.cpp
+++ Z3MBot2022_Timed/src/main/cpp/Auton.cpp
@@ -25,6 +25,12 @@ void Auton::Init() {
     }
     currentState = 0;
     stateStartTime = GetTime();
+    turnSpeed = 0.8;
+    driveSpeed = 0.8;
+
+    frc::SmartDashboard::PutNumber("Auton/Current State Time", GetTime() - stateStartTime);
+    frc::SmartDashboard::PutNumber("Auton/Drive Speed", driveSpeed);
+    frc::SmartDashboard::PutNumber("Auton/Turn Speed", turnSpeed);
 }
 
 void Auton::Periodic() {
@@ -46,11 +52,21 @@ void Auton::Periodic() {
         case 3:
             ShootTwo_Auton();
             break;
+        case 4:
+            TestDistance_Auton();
+            break;
+        case 5:
+            TestAngle_Auton();
+            break;
         default:
             m_autonSelectedNumber = 0;
             Idle_Auton();
             break;
     }
+
+    frc::SmartDashboard::PutNumber("Auton/Current State Time", GetTime() - stateStartTime);
+    frc::SmartDashboard::PutNumber("Auton/Drive Speed", driveSpeed);
+    frc::SmartDashboard::PutNumber("Auton/Turn Speed", turnSpeed);
 }
 
 double Auton::GetTime() {
@@ -60,6 +76,8 @@ double Auton::GetTime() {
 void Auton::GoToNextState() {
     stateStartTime = 0.0;
     currentState++;
+    turnSpeed = 0.8;
+    driveSpeed = 0.8;
 }
 
 void Auton::StopSubsystems(bool drive,  bool shooter, bool intake, bool wrist, bool tunnel) {
@@ -80,6 +98,41 @@ void Auton::StopSubsystems(bool drive,  bool shooter, bool intake, bool wrist, b
     }
 }
 
+// Return true if angle has been turned
+// Set turnSpeed depending on time since state start - to have a smooth slowdown
+// Degree passed in should be -180 to 180
+bool Auton::TurnAngle(double deg) {
+    double totalTurnTime = (deg/180.0)*kTimeToRotateHalf;
+    double currentTurnTime = GetTime() - stateStartTime;
+    if (currentTurnTime >= totalTurnTime) {
+        turnSpeed = 0.0;
+        return true;
+    } else if (currentTurnTime >= totalTurnTime*0.95) {
+        turnSpeed = deg < 0 ? -0.2 : 0.2;
+    } else {
+        turnSpeed = deg < 0 ? -0.3 : 0.3;
+        turnSpeed = turnSpeed*(totalTurnTime/currentTurnTime);
+    }
+    return false;
+}
+
+// Return true if distance has been driven
+// Set turnSpeed depending on time since state start
+// If dist < 0, drivespeed will be set to negative
+bool Auton::DriveDistance(double dist) {
+    double totalDriveTime = dist*kTimeToDrive1Foot;
+    double currentDriveTime = GetTime() - stateStartTime;
+    if (currentDriveTime >= totalDriveTime) {
+        driveSpeed = 0.0;
+        return true;
+    } else if (currentDriveTime >= totalDriveTime*0.95) {
+        driveSpeed = dist < 0 ? -0.3 : 0.3;
+    } else if (currentDriveTime >= totalDriveTime*0.75) {
+        driveSpeed = dist < 0 ? -0.6 : 0.6;
+    }
+    return false;
+}
+
 void Auton::Idle_Auton() {
     StopSubsystems(true, true, true, true, true);
 }
@@ -88,13 +141,13 @@ void Auton::Taxi_Auton() {
     // FACE FIELD/CARGO FOR TELEOP STARTING
     switch(currentState) {
         case 0: // Drive forwards
-            m_drive->ArcadeDrive(0.5, 0);
+            m_drive->ArcadeDrive(driveSpeed, 0);
             StopSubsystems(false, true, true, true, true);
             if (GetTime() - stateStartTime >= 5.0) {
                 GoToNextState();
             }
             break;
-        case 2: // Stop all motors
+        case 1: // Stop all motors
             StopSubsystems(true, true, true, true, true);
             break;
         default:
@@ -108,7 +161,7 @@ void Auton::ShootAndTaxi_Auton() {
     switch(currentState) {
         case 0: // Drive backwards and run shooter
             m_shooter->Run();
-            m_drive->ArcadeDrive(-0.5, 0);
+            m_drive->ArcadeDrive(-driveSpeed, 0);
             StopSubsystems(false, false, true, true, true);
             if (GetTime() - stateStartTime >= 3) {
                 GoToNextState();
@@ -137,14 +190,14 @@ void Auton::ShootTwo_Auton() {
         case 0: // Drive forward and run intake
             m_intake->MoveWristDown();
             m_intake->RunRoller();
-            m_drive->ArcadeDrive(0.5, 0);
+            m_drive->ArcadeDrive(driveSpeed, 0);
             StopSubsystems(false, true, false, false, true);
             if (GetTime() - stateStartTime >= 3) {
                 GoToNextState();
             }
             break;
         case 1: // Turn around, ramp up shooter and pull up wrist
-            m_drive->ArcadeDrive(0, 0.6);
+            m_drive->ArcadeDrive(0, turnSpeed);
             m_shooter->Run();
             m_intake->MoveWristUp();
             StopSubsystems(false, false, true, false, true);
@@ -162,6 +215,56 @@ void Auton::ShootTwo_Auton() {
             }
             break;
         case 3: // Stop all motors
+            StopSubsystems(true, true, true, true, true);
+            break;
+        default:
+            StopSubsystems(true, true, true, true, true);
+            break;
+    }
+}
+
+void Auton::TestDistance_Auton() {
+    switch(currentState) {
+        case 0: // Drive backwards 10 feet
+            m_drive->ArcadeDrive(driveSpeed, 0);
+            StopSubsystems(false, true, true, true, true);
+            if (DriveDistance(-10.0)) {
+                GoToNextState();
+            }
+            break;
+        case 1: // Drive forward 5 feet
+            m_drive->ArcadeDrive(driveSpeed, 0);
+            StopSubsystems(false, true, true, true, true);
+            if (DriveDistance(5.0)) {
+                GoToNextState();
+            }
+            break;
+        case 2: // Stop all motors
+            StopSubsystems(true, true, true, true, true);
+            break;
+        default:
+            StopSubsystems(true, true, true, true, true);
+            break;
+    }
+}
+
+void Auton::TestAngle_Auton() {
+    switch(currentState) {
+        case 0: // Rotate -60
+            m_drive->ArcadeDrive(0, turnSpeed);
+            StopSubsystems(false, true, true, true, true);
+            if (TurnAngle(-60)) {
+                GoToNextState();
+            }
+            break;
+        case 1: // Rotate 120
+            m_drive->ArcadeDrive(0, turnSpeed);
+            StopSubsystems(false, true, true, true, true);
+            if (TurnAngle(120)) {
+                GoToNextState();
+            }
+            break;
+        case 2: // Stop all motors
             StopSubsystems(true, true, true, true, true);
             break;
         default:
--- Z3MBot2022_Timed/src/main/include/Auton.h
+++ Z3MBot2022_Timed/src/main/include/Auton.h
@@ -22,6 +22,8 @@ class Auton {
     void Taxi_Auton();
     void ShootAndTaxi_Auton();
     void ShootTwo_Auton();
+    void TestDistance_Auton();
+    void TestAngle_Auton();
 
     public:
     Auton(Drive* drive, Shooter* shooter, Intake* intake, Tunnel* tunnel);
@@ -31,13 +33,21 @@ class Auton {
     double GetTime();
     void GoToNextState();
     void StopSubsystems(bool drive,  bool shooter, bool intake, bool wrist, bool tunnel);
+    bool TurnAngle(double deg);
+    bool DriveDistance(double dist);
     
     std::string m_autonSelectedString;
     int m_autonSelectedNumber;
     int currentState;
     double stateStartTime;
+    double driveSpeed;
+    double turnSpeed;
     frc::Timer m_timer;
 
     // Update this vector when you add more routines
-    const std::vector<std::string> kAutoNames{"Idle", "Taxi", "Shoot And Taxi", "Shoot Two"};
+    const std::vector<std::string> kAutoNames{"Idle", "Taxi", "Shoot And Taxi", "Shoot Two", "Test Distance", "Test Angle"};
+
+    // CHANGE THESE WITH TESTING
+    const double kTimeToRotateHalf = 2.0;
+    const double kTimeToDrive1Foot = 2.0;
 };
